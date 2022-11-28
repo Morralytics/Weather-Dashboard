a24d96725e1529770445be49f81fea045a48c563
Nick Morris <nicholasmorris2599@gmail.com>
2022-11-27 17:01:10 -0800

    Clears previous weather information on new search entry.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,12 @@ $(function() {
         numOfSearches += 1;       
 
         localStorage.setItem(JSON.stringify(numOfSearches), userInput);
+
+        if ($('.card temp') !== null) {
+            clearCurrentWeatherInfo();
+        }
         printHistory(userInput);
+        printCityWeatherInfo(userInput);
     });
 
     var setStorage = function() {
@@ -34,6 +39,32 @@ $(function() {
         listEl.addClass('list-text').text(listDetail);
         bootstrapDiv.appendTo(historySection);
         listEl.appendTo(bootstrapDiv);
+    }
+
+    var printCityWeatherInfo = function(city) {
+        var placementDiv = $('.weather-info');
+        var bootstrapDiv = $('<div>');
+        var headerDiv = $('<div>');
+        var bodyDiv = $('<div>');
+        var cardTitle = $('<h5>');
+        var cardText = $('<p>');
+        var listDetail = city;
+
+        bootstrapDiv.addClass('card temp');
+        headerDiv.addClass('card-header temp').text('Search for a city to get a full 5 day forcast!');
+        bodyDiv.addClass('card-body temp');
+        cardTitle.addClass('card-title temp').text(listDetail);
+        cardText.addClass('card-text temp').text('Temp, wind, humidity placeholder');
+
+        bootstrapDiv.appendTo(placementDiv);
+        headerDiv.appendTo(bootstrapDiv);
+        bodyDiv.appendTo(bootstrapDiv);
+        cardTitle.appendTo(bodyDiv);
+        cardText.appendTo(bodyDiv);
+    }
+
+    var clearCurrentWeatherInfo = function() {
+        $('.weather-info').empty();
     }
 
     setStorage();
