812994db9b3c6fd96a6de2ae3b938965c28f744d
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-07 18:56:42 -0800

    Created new function that passes in all values from the data and uses it to create the daily forcast values.

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,30 @@ $(function() {
     // This is where the information on the API is stored as well as logging the information given
     var printCityWeatherInfo = function(city) {
         var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=1a91782a2d8a6d880d1f0a1bb5990c24`;
+        
+        
+        fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            var tempF = filterApiObj(data).main.temp;
+            var wind = filterApiObj(data).wind.speed;
+            var humidity = filterApiObj(data).main.humidity;
+            var icon = `https://openweathermap.org/img/w/${filterApiObj(data).weather[0].icon}.png`
+            console.log(tempF)
+            dailyCardRender(city, tempF, wind, humidity, icon);
+        });
+
+    }
+
+    // This function clears any items with room for updated items in local storage
+    var clearCurrentWeatherInfo = function() {
+        $('.weather-info').empty();
+    }
+
+    var dailyCardRender = function(city, temp, wind, humidity, icon) {
         var placementDiv = $('.weather-info');
         var bootstrapDiv = $('<div>');
         var bootstrapDiv2 = $('<div>')
@@ -58,45 +82,17 @@ $(function() {
         var bodyDiv2 = $('<div>');
         var cardTitle = $('<h5>');
         var cardText = $('<p>');
+        var weatherIcon = $('<img>');
         var listDetail = city;
-        
-        fetch(requestUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            var tempF = filterApiObj(data).main.temp
-            console.log(tempF)
-            
-            // This is what I was not able to complete
-            // Through research and API documentation I was unable to figure out how to grab API information on a daily basis rather than a 3 hour basis
-            // I could have just grabbed from an index however, with variation of time based on when the user is using the cite, it would not work
-            // I also though of using a specific index at a specific time to use that information however if a user used the cite AFTER that time slot, it would push to the next day which would not work
-            // There were a lot of factors that I wasn't able to work out on paper
-            // for (i = 0; i < data.list.length; i++) {
-            //     var arr = data.list[i].dt_txt.split([''])
-            //     var time = arr[11] + arr[12];
-            //     var date = data.list[i].dt_txt.split(' ')[0];
-            //     var tmrDate = data.list[i].dt_txt.split(' ')[0]+1;
-            //     var timeStamp = new Date(data.list[i].dt * 1000);
-            //     console.log(timeStamp);
-            // }
-
-            // This logs the current time and date that I was going to use to compare to the items from the API
-            // var time = new Date();
-            // var localTime = time.getTime();
-            // var localOffset = time.getTimezoneOffset() * 60000;
-            // var utc = localTime + localOffset;
-            // var userCity = utc +(1000 * data.city.timezone);
-            // console.log(new Date(userCity));
-            // cardText.addClass('card-text temp').text(data);
-        });
 
         bootstrapDiv.addClass('card temp');
         headerDiv.addClass('card-header temp').text('Search another city to get a full 5 day forcast!');
         bodyDiv.addClass('card-body temp');
         cardTitle.addClass('card-title temp').text(listDetail);
+        cardTitle.addClass('card-title temp').text(temp);
+        cardTitle.addClass('card-title temp').text(wind);
+        cardTitle.addClass('card-title temp').text(humidity);
+        weatherIcon.attr('src', icon);
 
         bootstrapDiv2.addClass('card smaller temp');
         bodyDiv2.addClass('card-body smaller temp').text('Placeholder');
@@ -106,16 +102,12 @@ $(function() {
         bodyDiv.appendTo(bootstrapDiv);
         cardTitle.appendTo(bodyDiv);
         cardText.appendTo(bodyDiv);
+        weatherIcon.appendTo(placementDiv);
 
         for (i = 0; i < 3; i ++) {
             bootstrapDiv2.appendTo(placementDiv);
             bodyDiv2.appendTo(bootstrapDiv2);
         }
-    }
-
-    // This function clears any items with room for updated items in local storage
-    var clearCurrentWeatherInfo = function() {
-        $('.weather-info').empty();
     }
 
     var filterApiObj = function(data) {
